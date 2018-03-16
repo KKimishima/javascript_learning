59b9081a18c3b234f92f01d898b6a9f52cfd1e53
君島克弥 <K.Kimishima123@gmail.com>
2018-03-16 23:59:43 +0900

    <追加>モクモク追加

--- 5.DOM/src/dom.js
+++ 5.DOM/src/dom.js
@@ -47,8 +47,115 @@ export function D7() {
   }, false)
 }
 
-export let onclickEvent = document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('d7').addEventListener('click', () => {
-    window.alert("イベントリスナー")
+
+export function D8() {
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('d7').addEventListener('click', () => {
+      window.alert("イベントリスナー")
+    }, false)
+  }, false);
+}
+
+export function D9() {
+  // let re = document.getElementById("d1")
+  let re = document.getElementById("d9").getAttribute("href")
+  document.getElementById("d9").setAttribute("href", "http://fooss")
+  let re2 = document.getElementById("d9").getAttribute("href")
+  // let p = re.getAttribute("p")
+  console.log(re2)
+}
+
+export function D10() {
+  let re = document.getElementById("d10")
+  let map = re.attributes
+  for (const d of map) {
+    console.log(d)
+  }
+}
+
+export function D11() {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("d11-0").addEventListener("click", () => {
+      document.getElementById("d11-1").textContent =
+        '<a href="http://hoge">hoge</a>';
+      document.getElementById("d11-2").innerHTML =
+        '<a href="http://hoge">hoge</a>';
+    }, false)
   }, false)
-}, false);
+}
+
+export function D12() {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("d12").addEventListener("click", () => {
+      window.alert("hoge")
+      document.getElementById("d12-1").textContent =
+        document.getElementById("d12-2").value
+    }, false)
+  }, false)
+}
+
+export function D13() {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("d13-0").addEventListener("click", () => {
+      let re = document.getElementsByName("food13")
+      let ary = []
+      for (let d of re) {
+        if (d.checked)
+          ary.push(d.value)
+      }
+      window.alert(ary.toLocaleString())
+    }, false)
+  }, false)
+}
+
+export function D14() {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("d14-0").addEventListener("click", () => {
+      let re = document.getElementsByName("food14")
+      let r = "";
+      for (let d of re) {
+        if (d.checked)
+          r = d.value;
+        // break;
+      }
+
+      if (r === "") {
+        window.alert("チェックがついていいません")
+      } else {
+        window.alert(`${r}が選択されました`)
+      }
+
+    }, false)
+  }, false)
+}
+
+export function D15() {
+  document.addEventListener("DOMContentLoaded", () => {
+    let setCheck = (name, value) => {
+      let elem = document.getElementsByName(name)
+      for (const d of elem) {
+        if (value.indexOf(d.value) > -1) {
+          d.checked = true
+        }
+      }
+    }
+    setCheck("food15", ["ラーメン", "カレー"])
+  }, false)
+}
+
+export function D16() {
+  window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("d16").addEventListener("change", (e) => {
+      window.alert("hoge")
+      let inFile = document.getElementById("d16").files
+      let re = [];
+      for (let i of inFile) {
+        re.push(`ファイル名:${i.name}`)
+        re.push(`ファイル種類:${i.type}`)
+        re.push(`ファイル名:${i.size / 1024}KB`)
+        re.push(`ファイルの最終更新日:${i.lastModifiedDate}`)
+      }
+      window.alert(re.toString())
+    }, true)
+  })
+}
--- 5.DOM/src/index.js
+++ 5.DOM/src/index.js
@@ -5,7 +5,14 @@ import { D4 } from "./dom"
 import { D5 } from "./dom"
 import { D6 } from "./dom"
 // import { D7 } from "./dom"
-import { onclickEvent } from "./dom"
+import { D8 } from "./dom"
+import { D9 } from "./dom"
+import { D10 } from "./dom"
+import { D11 } from "./dom"
+import { D12 } from "./dom"
+import { D13 } from "./dom"
+import { D14 } from "./dom"
+import { D15 } from "./dom"
 
 let d1 = D1
 let d2 = D2
@@ -13,9 +20,22 @@ let d3 = D3
 let d4 = D4
 let d5 = D5
 let d6 = D6
+let d8 = D8
+let d9 = D9
+let d10 = D10
+let d11 = D11
+let d12 = D12
+let d13 = D13
+let d14 = D14
+let d15 = D15
 // let d7 = D7
 
-onclickEvent
+d8()
+d11()
+d12()
+d13()
+d14()
+d15()
 
 window.onload = () => {
   d1()
@@ -24,4 +44,6 @@ window.onload = () => {
   d4()
   d5()
   d6()
+  d9()
+  d10()
 }
